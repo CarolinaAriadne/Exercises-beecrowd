fed9631ca67f34e61e123624f3531de570a8fd15
CarolinaAriadne <c.ariadneadv@gmail.com>
2023-02-27 15:53:52 -0300

    page-tasks-deleteTask-complete

---
+++ teste40.js
@@ -0,0 +1,13 @@
+let i = 1;
+let j = 5;
+
+for (let index = 1; index <= 10; index +=2) {
+ 
+  console.log(`I=${index} J=${(j = j + 2)}`);
+  j = j - 1;
+  console.log(`I=${index} J=${j}`);
+  j = j - 1;
+  console.log(`I=${index} J=${j}`);
+
+
+}
